9846efe5ea90540449e2cdaa6395a612255f7630
CAYETANOT Pierre-Olivier <pierre-olivier.cayetanot@cresym.eu>
2026-04-07 17:35:58 +0200

    Basic boiler plate initialisation

---
+++ src/boilerplate.py
@@ -0,0 +1,12 @@
+def add(a: int, b: int) -> int:
+    """
+    Add two integers.
+
+    Args:
+        a: First integer.
+        b: Second integer.
+
+    Returns:
+        The sum of a and b.
+    """
+    return a + b
